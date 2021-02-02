22bbdc14473e2c9049be1ec4001381d8c6dae4f5
RogueNPC <jordanbtansay@gmail.com>
2021-02-01 21:28:15 -0800

    replaced music-playlist to linked list instead of array implementation

--- playlist.py
+++ playlist.py
@@ -3,36 +3,41 @@ from song import Song
 class Playlist:
   def __init__(self):
     self.__first_song = None
-    self.__song_list = []
 
 
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    song = Song(title)
-    self.__song_list.append(song)
+
+    # Create a new song (node)
+    new_song = Song(title)
+
+    # Set next pointer to first node
+    new_song.__next_song = self.__first_song
+    self.__first_song = new_song
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    for index in range(self.length()):
-      if self.__song_list[index] == title:
-        return index
-    return -1
+    current_song = self.__first_song
+    while current_node.title != title:
+      current_node = current_node.__next_song
+      if current_node = None:
+        return -1
+    return 
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    index = self.find_song(title)
-    del self.__song_list[index]
+    pass
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    return len(self.__song_list)
+    pass
 
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
@@ -44,5 +49,4 @@ class Playlist:
 
   def print_songs(self):
     pass
-
-  
+    
