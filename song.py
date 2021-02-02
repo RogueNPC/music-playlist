a440c33c4083c5322e0ed45885b461f2b7a9d611
RogueNPC <jordanbtansay@gmail.com>
2021-02-01 18:25:12 -0800

    added Song __str__ and __repr__ methods

--- song.py
+++ song.py
@@ -27,9 +27,9 @@ class Song:
 
   # TODO: Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
-    pass
+    self.get_title()
 
 
   # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
   def __repr__(self):
-    pass
+    f'{self.get_title()} -> {self.get_next_song()}'
